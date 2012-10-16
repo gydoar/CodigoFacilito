7137ab84b31cd73c34a95c25fc5d53574e58c87b
Henoc Dz <tutoriales@roofdier.com>
2012-10-15 19:37:27 -0500

    Alpha 0.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codigofacilito::Application.config.secret_token = 'b1c7f8b5d0fef972d3966eb3939c10302ab7880a42997c990a6f780eec372a6a5052ad1800d576da2ffda10e73ed6a8229f055e02e33d50c1bfb2aaef50655ce'
