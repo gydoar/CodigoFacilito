895fa0e097878cf1599c0272eacd381a1a9b00dc
Marcos Uriel Hernández Camacho <uriel.hdzc@gmail.com>
2012-10-15 19:50:00 -0500

    Agregar thumbnails al Index

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'youtube_it'
+gem 'youtube_search'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,8 @@
 class AppController < ApplicationController
 	def index
+		client = YouTubeIt::Client.new(:dev_key => "AI39si5Oce1nyi_aXOAFVoD8j0aztUkuH1ZmvTfCvme5h-TPbuJD-NexHVuRuBP2U-2mK144ruhuEEfCwT5cDATsfGBo6ufc0Q")
+  	@cf = client.profile("codigofacilito")
+  	@sus = client.playlists("codigofacilito")
+  	@playlist = client.playlist(@sus[0].playlist_id)
 	end
 end
