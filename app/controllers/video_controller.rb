127a46db9a48d3533d39abcdfc706cfaff3c6f64
Marcos Uriel Hernández Camacho <uriel.hdzc@gmail.com>
2012-10-15 22:20:52 -0500

    Se agrego temario

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -6,5 +6,13 @@ class VideoController < ApplicationController
 		client = YouTubeIt::Client.new(:dev_key => "AI39si5Oce1nyi_aXOAFVoD8j0aztUkuH1ZmvTfCvme5h-TPbuJD-NexHVuRuBP2U-2mK144ruhuEEfCwT5cDATsfGBo6ufc0Q")
 		@video = client.video_by(params[:id])
 		@sus = client.playlists("codigofacilito")
+		@sus.each do |playlist|
+			pl = client.playlist(playlist.playlist_id)
+			pl.videos.each do |video|
+				if video.unique_id == params[:id]
+					@current_playlist = pl
+				end
+			end
+		end
 	end
 end
