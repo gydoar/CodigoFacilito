ba897475b3c5f0f380769c50269f973e7695f796
Marcos Uriel Hernández Camacho <uriel.hdzc@gmail.com>
2012-10-16 21:59:09 -0500

    Se agregaron validaciones

--- app/controllers/cursos_controller.rb
+++ app/controllers/cursos_controller.rb
@@ -1,7 +1,18 @@
-class CursosController < ApplicationController	
+class CursosController < ApplicationController
+	def index
+		redirect_to root_path
+	end	
 	def show
 		client = YouTubeIt::Client.new(:dev_key => "AI39si5Oce1nyi_aXOAFVoD8j0aztUkuH1ZmvTfCvme5h-TPbuJD-NexHVuRuBP2U-2mK144ruhuEEfCwT5cDATsfGBo6ufc0Q")		
-		@playlist = client.playlist(params[:id])
+		begin
+			@playlist = client.playlist(params[:id])
+			if @playlist.nil?
+				redirect_to root_path
+			end
+		rescue Exception => e
+			redirect_to root_path
+		end
+		
 		@sus = client.playlists("codigofacilito")
 	end
 
--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,10 +1,15 @@
 class VideoController < ApplicationController
 	def index
-		
-	end
+		redirect_to root_path
+	end	
 	def show
 		client = YouTubeIt::Client.new(:dev_key => "AI39si5Oce1nyi_aXOAFVoD8j0aztUkuH1ZmvTfCvme5h-TPbuJD-NexHVuRuBP2U-2mK144ruhuEEfCwT5cDATsfGBo6ufc0Q")
-		@video = client.video_by(params[:id])
+		begin
+			@video = client.video_by(params[:id])	
+		rescue Exception => e
+			redirect_to root_path	
+		end
+		
 		@sus = client.playlists("codigofacilito")
 		@sus.each do |playlist|
 			pl = client.playlist(playlist.playlist_id)
