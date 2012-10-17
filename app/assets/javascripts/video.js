496e448e2d29cc1c9cc495f1fe454ad6d342844d
Henoc Dz <tutoriales@roofdier.com>
2012-10-16 21:14:31 -0500

    Texto "Ver/Ocultar temario" boton

--- app/assets/javascripts/video.js
+++ app/assets/javascripts/video.js
@@ -2,11 +2,15 @@ $(function(){
 	var visible = false;
 	topics = $("#topics");
 	$('#show_topics').on('click',function(){
+
+		var s = $(this);
 		if(visible){
 			topics.slideUp();
+			s.text('Ver temario');
 			visible = false;
 		}else{
 			visible = true;
+			s.text('Cerrar temario');
 			topics.slideDown();
 		}
 	})
