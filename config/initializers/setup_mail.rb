626fb8a8548b0515d30716748751d51705c683e3
Marcos Uriel Hernández Camacho <uriel.hdzc@gmail.com>
2012-12-16 14:18:32 +0100

    Cambiar direccion mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "codigofacilito.com",
-  :user_name            => "uriel.hdzc",
-  :password             => "Dxv82727",
+  :user_name            => "codigofacilito.contacto",
+  :password             => "facilito123",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
