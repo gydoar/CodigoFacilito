26deedcc1a430d8d1448cf932a2cc42d051fbd70
Marcos Uriel Hernández Camacho <uriel.hdzc@gmail.com>
2012-12-16 14:23:05 +0100

    Reestablecer correo

--- app/mailers/cf_mailer.rb
+++ app/mailers/cf_mailer.rb
@@ -13,6 +13,6 @@ class CfMailer < ActionMailer::Base
     if(data[:url])
       @url = data[:url]
     end
-    mail(:to => "projectdxv@gmail.com", :subject => "Contacto en Codigo Facilito")
+    mail(:to => "uriel.hdzc@gmail.com ", :subject => "Contacto en Codigo Facilito")
   end
 end
